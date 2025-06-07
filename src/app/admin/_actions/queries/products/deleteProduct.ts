7e20c08ae302ed3d4cf7a753d8fb2f339bdcf615
richazim <saibouazim90@gmail.com>
2025-06-07 20:17:06 +0100

    delete product fixed

--- src/app/admin/_actions/queries/products/deleteProduct.ts
+++ src/app/admin/_actions/queries/products/deleteProduct.ts
@@ -4,15 +4,19 @@ import db from "@/db/db"
 import fs from "fs/promises"
 import { notFound } from "next/navigation"
 import { revalidatePath } from "next/cache"
+import { extractFilenameFromPath } from "@/app/admin/_lib/utils/filename"
 
 
 export async function deleteProduct(id: string) {
     const product = await db.product.delete({ where: { id } })
   
     if (product == null) return notFound()
-  
-    await fs.unlink(product.filePath)
-    await fs.unlink(`public${product.imagePath}`)
+    
+    const realFilePath = extractFilenameFromPath(product.filePath)
+    const realImagePath = extractFilenameFromPath(product.imagePath)
+
+    await fs.unlink(realFilePath)
+    await fs.unlink(realImagePath)
   
     revalidatePath("/")
     revalidatePath("/products")
