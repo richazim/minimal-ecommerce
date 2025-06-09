49b4af1bd60f1350323a866f8d36a3954653af1e
richazim <saibouazim90@gmail.com>
2025-06-09 22:34:40 +0100

    some fixes

--- src/app/(customer)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customer)/products/download/[downloadVerificationId]/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server"
 import { getValidDownloadVerification } from "@/lib/queries/download-verification/getValidDownloadVerification"
 
+export const dynamic = 'force-dynamic';
+
+
 export async function GET(
   req: NextRequest,
   { params }: { params: Promise<{ downloadVerificationId: string }> }
